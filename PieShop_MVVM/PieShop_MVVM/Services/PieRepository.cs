941145c79debc5f3ea20918e1d3d82f6da87a4cb
Michiel Wouters <michiel_woute64@hotmail.com>
2021-04-15 15:59:44 +0200

    End of lesson 1

--- PieShop_MVVM/PieShop_MVVM/Services/PieRepository.cs
+++ PieShop_MVVM/PieShop_MVVM/Services/PieRepository.cs
@@ -5,16 +5,29 @@ namespace PieShop_MVVM.Services
 {
     public class PieRepository : IPieRepository
     {
+        // SINGLETON
+        private static PieRepository instance;
+
+        // Private constructor -> No outside instancing of this class
+        private PieRepository()
+        {
+            AddDummyData();
+        }
+
+        // Static method. Other classes will request the one object in this class
+        // If it does not exist yet, create it first.
+        public static PieRepository GetSingleton()
+        {
+            if (instance == null)
+            {
+                instance = new PieRepository();
+            }
+
+            return instance;
+        }
+
         // MOCK Database
         private List<Pie> pies;
-
-        public PieRepository()
-        {
-            if (pies == null)
-            {
-                AddDummyData();
-            }
-        }
 
         public List<Pie> GetAllPies()
         {
@@ -62,6 +75,11 @@ namespace PieShop_MVVM.Services
                         Description = "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies."
                     }
                 };
+        }
+
+        public void AddPie(Pie pie)
+        {
+            pies.Add(pie);
         }
     }
 };
