c215a0b8a191a674b18c70d2b98112fa7a48ea98
Michiel Wouters <michiel_woute64@hotmail.com>
2021-04-15 13:36:04 +0200

    Added Add command

--- PieShop_MVVM/PieShop_MVVM/ViewModels/PieOverviewViewModel.cs
+++ PieShop_MVVM/PieShop_MVVM/ViewModels/PieOverviewViewModel.cs
@@ -2,20 +2,55 @@
 using PieShop_MVVM.Services;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace PieShop_MVVM.ViewModels
 {
-    public class PieOverviewViewModel
+    public class PieOverviewViewModel : BaseViewModel
     {
         // Always initialize lists, even when empty
-        public ObservableCollection<Pie> Pies { get; set; } = new ObservableCollection<Pie>();
+        private ObservableCollection<Pie> pies;
+
+        public ObservableCollection<Pie> Pies
+        {
+            get { return pies; }
+            set
+            {
+                pies = value;
+                OnPropertyChanged(nameof(Pies));
+            }
+        }
+
 
         private IPieRepository repository;
+
+        public ICommand AddPieCommand { get; }
 
         public PieOverviewViewModel()
         {
             repository = new PieRepository();
 
+            RefreshPies();
+            AddPieCommand = new Command(AddPie);
+        }
+
+        private void AddPie()
+        {
+            var pie = new Pie
+            {
+                Description = "I am moist and delicious",
+                ImageUrl = "strawberrypiesmall.jpg",
+                IsInStock = true,
+                Name = "New Pie",
+                Price = 13.95
+            };
+
+            Pies.Add(pie);
+        }
+
+        private void RefreshPies()
+        {
             List<Pie> pies = repository.GetAllPies();
             Pies = new ObservableCollection<Pie>(pies);
         }
