8a30ac9618d86d74114c68ef2a602de9727574be
Michiel Wouters <michiel_woute64@hotmail.com>
2021-04-16 14:25:49 +0200

    Add DbContext using Sqlite and EF

---
+++ PieShop_MVVM/PieShop_MVVM/Services/PieshopContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PieShop_MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace PieShop_MVVM.Services
+{
+    public class PieshopContext: DbContext
+    {
+        public DbSet<Coffee> Coffees { get; set; }
+
+        public DbSet<Pie> Pies { get; set; }
+
+        public PieshopContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "PieShop.sqlite");
+            optionsBuilder.UseSqlite($"FileName = {dbPath}");
+        }
+    }
+}
